02270a7818da4625ff239264a0cf962ac5c6426e
PRINCE ROY <83387075+princeroy8606@users.noreply.github.com>
2024-08-12 10:05:08 +0530

    Update NewKnowledge.jsx

--- src/components/KnowledgeHub/NewKnowledge.jsx
+++ src/components/KnowledgeHub/NewKnowledge.jsx
@@ -117,8 +117,7 @@ const NewKnowledge = ({
   const getVideoId = (url) => {
     const regex =
       /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
-    const match = url.match(regex);
-    console.log(match[1]);
+    const match = url?.match(regex);
     return match ? match[1] : null;
   };
 
